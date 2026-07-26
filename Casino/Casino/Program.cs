e1b458e51be5ddcb847b39ab20ce2e5c187a4277
Вячеслав Меркушев <vamerkushev@yandex.ru>
2026-07-27 02:28:00 +0300

    Casino: Внес правки по ревью: переименовал .editorconfig и проверил форматирование, убрал .gitignore, избавился от дублирования кода, добавил валидацию и более удобную проверку ввода, switch-case стал чище

--- Casino/Casino/Program.cs
+++ Casino/Casino/Program.cs
@@ -1,36 +1,26 @@
-﻿var printTitle = new[]
+﻿void PrintIntro()
 {
-    "######       ###        #####     ######     ##   ##      #### ",
-    "##          ## ##      ##           ##       ###  ##     ##  ##",
-    "##         ##   ##     ######       ##       ## # ##     ##  ##",
-    "##         #######         ##       ##       ##  ###     ##  ##",
-    "######     ##   ##     #####      ######     ##   ##      #### "
-};
-
-foreach ( var line in printTitle )
-{
-    Console.WriteLine( line );
+    Console.WriteLine( """
+                  ######       ###        #####     ######     ##   ##      ####
+                  ##          ## ##      ##           ##       ###  ##     ##  ##
+                  ##         ##   ##     ######       ##       ## # ##     ##  ##
+                  ##         #######         ##       ##       ##  ###     ##  ##
+                  ######     ##   ##     #####      ######     ##   ##      ####
+                  """ );
 }
 
-Console.WriteLine();
-Console.WriteLine( "Привет, чемпион! Ознакомься с меню игры и выбери действие (Введи число):" );
-Console.WriteLine( "1. Ввести депозит" );
-Console.WriteLine( "2. Посмотреть баланс" );
-Console.WriteLine( "3. Сделать ставку" );
-Console.WriteLine( "4. Выйти из игры" );
-Console.WriteLine( "5. Меню команд" );
-
-int balance = 0;
-const int multiplicator = 2;
-
-string command = string.Empty;
-command = Console.ReadLine() ?? string.Empty;
-while ( ParseCommand( command ) )
+void PrintMenu()
 {
-    command = Console.ReadLine() ?? string.Empty;
+    Console.WriteLine();
+    Console.WriteLine( "Привет, чемпион! Ознакомься с меню игры и выбери действие (Введи число):" );
+    Console.WriteLine( "1. Ввести депозит" );
+    Console.WriteLine( "2. Посмотреть баланс" );
+    Console.WriteLine( "3. Сделать ставку" );
+    Console.WriteLine( "4. Выйти из игры" );
+    Console.WriteLine( "5. Меню команд" );
 }
 
-int ReadDeposit()
+int CheckValidInt()
 {
     while ( true )
     {
@@ -43,8 +33,7 @@ int ReadDeposit()
     }
 }
 
-
-void GameIteration()
+void GameIteration( ref int balance, int multiplicator )
 {
     if ( balance <= 0 )
     {
@@ -52,12 +41,7 @@ void GameIteration()
         return;
     }
 
-    string betStr = Console.ReadLine() ?? string.Empty;
-    if ( !int.TryParse( betStr, out int bet ) )
-    {
-        Console.WriteLine( "Введите целое положительное число!" );
-        return;
-    }
+    int bet = CheckValidInt();
 
     if ( bet > balance )
     {
@@ -65,20 +49,14 @@ void GameIteration()
         return;
     }
 
-    if ( bet <= 0 )
-    {
-        Console.WriteLine( "Введите целое положительное число!" );
-        return;
-    }
-
-    int diff = CalcBalanceDiff( bet );
+    int diff = CalcBalanceDiff( bet, multiplicator );
     balance += diff;
 
     Console.WriteLine( diff > 0 ? "Победа!" : "Поражение!" );
     Console.WriteLine( $"Ваш новый баланс: {balance}" );
 }
 
-int CalcBalanceDiff( int bet )
+int CalcBalanceDiff( int bet, int multiplicator )
 {
     int randomNum = Random.Shared.Next( 1, 21 );
     bool isWon = randomNum >= 18;
@@ -86,35 +64,57 @@ int CalcBalanceDiff( int bet )
     return isWon ? bet * ( 1 + ( multiplicator * randomNum % 17 ) ) : -bet;
 }
 
-bool ParseCommand( string command )
+void MakeDeposit( ref int balance )
 {
-    switch ( command )
+    Console.WriteLine( $"Введите ваш депозит:" );
+    int deposit = CheckValidInt();
+    balance += deposit;
+    Console.WriteLine( $"Счёт пополнен! Ваш текущий баланс: {balance}." );
+}
+
+void MakeBet( ref int balance, int multiplicator )
+{
+    Console.WriteLine( $"Введите ставку. Ваш текущий баланс: {balance}." );
+    GameIteration( ref balance, multiplicator );
+}
+
+void RunGame()
+{
+    int balance = 0;
+    const int multiplicator = 2;
+
+    while ( true )
     {
-        case "1":
-            Console.WriteLine( $"Введите ваш депозит:" );
-            int deposit = ReadDeposit();
-            balance += deposit;
-            Console.WriteLine( $"Счёт пополнен! Ваш текущий баланс: {balance}." );
-            return true;
-        case "2":
-            Console.WriteLine( $"Ваш баланс: {balance}." );
-            return true;
-        case "3":
-            Console.WriteLine( $"Введите ставку. Ваш текущий баланс: {balance}." );
-            GameIteration();
-            return true;
-        case "4":
-            Console.WriteLine( $"Спасибо за игру! Ваш конечный баланс: {balance}." );
-            return false;
-        case "5":
-            Console.WriteLine( "1. Ввести депозит." );
-            Console.WriteLine( "2. Посмотреть баланс." );
-            Console.WriteLine( "3. Сделать ставку." );
-            Console.WriteLine( "4. Выйти из игры." );
-            Console.WriteLine( "5. Меню команд." );
-            return true;
-        default:
-            Console.WriteLine( "Команда не найдена! Смотрите меню команд! ('5'-для просмотра меню)." );
-            return true;
+        string command = Console.ReadLine() ?? string.Empty;
+        switch ( command )
+        {
+            case "1":
+                MakeDeposit( ref balance );
+                break;
+            case "2":
+                Console.WriteLine( $"Ваш баланс: {balance}." );
+                break;
+            case "3":
+                MakeBet( ref balance, multiplicator );
+                break;
+            case "4":
+                Console.WriteLine( $"Спасибо за игру! Ваш конечный баланс: {balance}." );
+                return;
+            case "5":
+                PrintMenu();
+                break;
+            default:
+                Console.WriteLine( "Команда не найдена! Смотрите меню команд! ('5'-для просмотра меню)." );
+                break;
+        }
     }
 }
+
+void Main()
+{
+    PrintIntro();
+    PrintMenu();
+    RunGame();
+}
+
+Main();
